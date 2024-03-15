a4668586a5316de8c47df5b834128fde74f18886
DerickMghanga <derickmghanga3329@gmail.com>
2024-03-15 23:49:03 +0300

    Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
   return (
-    <div>
-      Hello world!!
-    </div>
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Your marketplace for high-quality{' '}
+          <span className="text-rose-600">digital assets</span>.
+        </h1>
+        
+        <p className="mt-6 text-lg max-w-prose">
+          Welcome to DigiHippo. Every asset on our platform is verified by our team to ensure our highest quality standards.
+        </p>
+      </div>
+    </MaxWidthWrapper>
   );
 }
