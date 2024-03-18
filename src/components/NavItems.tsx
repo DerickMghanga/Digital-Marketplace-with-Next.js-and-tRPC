75b9c27c6507a50f5d92efea3433f579745c8786
DerickMghanga <derickmghanga3329@gmail.com>
2024-03-18 23:45:52 +0300

    close navbar using esc key

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,48 +1,66 @@
 "use client"
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import NavItem from "./NavItem";
 import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 export default function NavItems() {
-    const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
-    const isAnyOpen  = activeIndex !== null  // means a user has selected one category
+  // close navbar menu using ESC key (escape key)
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null)
+      }
+    }
+    
+    // handler is called once a key is pressed >> add an event listener
+    document.addEventListener('keydown', handler)
 
-    //close navbar menu when user clicks outside the navbar
-    const navRef = useRef<HTMLDivElement | null>(null)
+    //clean-up function  >> prevents memory leaks
+    return () => {
+      document.removeEventListener("keydown", handler)
+    }
 
-    // from hooks/use-on-click-outside.ts >> takes in the ref element and function to execute
-    useOnClickOutside(navRef, ()=>setActiveIndex(null)) 
+  }, [])
+
+  const isAnyOpen = activeIndex !== null  // means a user has selected one category
+
+  //close navbar menu when user clicks outside the navbar
+  const navRef = useRef<HTMLDivElement | null>(null)
+
+  // from hooks/use-on-click-outside.ts >> takes in the ref element and function to execute
+  useOnClickOutside(navRef, () => setActiveIndex(null))
 
   return (
     <div className="flex gap-4 h-full" ref={navRef}>
-        {
-          PRODUCT_CATEGORIES.map((category, i) => {
+      {
+        PRODUCT_CATEGORIES.map((category, i) => {
 
-            // keep track of what item is active(clicked) on the NavBar
-            const handleOpen = () => {
-              if (activeIndex === i) {
-                setActiveIndex(null)  //close the menu if clicked again
-              } else {
-                setActiveIndex(i)
-              }
+          // keep track of what item is active(clicked) on the NavBar
+          const handleOpen = () => {
+            if (activeIndex === i) {
+              setActiveIndex(null)  //close the menu if clicked once again
+            } else {
+              setActiveIndex(i)
             }
+          }
 
-            const isOpen = i === activeIndex  //boolean
+          const isOpen = i === activeIndex  //boolean
 
-            return(
-              <NavItem
-                category={category}
-                handleOpen={handleOpen}
-                isOpen={isOpen}
-                key={category.value}
-                isAnyOpen={isAnyOpen}
-              />
-            )
-          })
-        }
+          return (
+            <NavItem
+              category={category}
+              handleOpen={handleOpen}
+              isOpen={isOpen}
+              key={category.value}
+              isAnyOpen={isAnyOpen}
+            />
+          )
+        })
+      }
     </div>
   )
 }
