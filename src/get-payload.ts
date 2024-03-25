e852713d98b3f29e6034bd3f6ffa1eac82b04f5e
DerickMghanga <derickmghanga3329@gmail.com>
2024-03-25 23:52:24 +0300

    custom express server set-up

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,10 +1,49 @@
 import dotenv from 'dotenv'
 import path from 'path' //in-built in node.js apps
+import payload from 'payload'
+import type { InitOptions } from 'payload/config'  // typescripts types for payload
 
 dotenv.config({
     path: path.resolve(__dirname, '../.env')
 })
 
-export const getPayloadClient = async() => {
+//caching to save resources in production
+let cached = (global as any).payload  //if cached version of payload CMS exists
 
+if (!cached) {
+    cached = (global as any).payload = {
+        client: null,
+        promise: null
+    }
+}
+
+interface Args {
+    initOptions?: Partial<InitOptions>
+}
+
+export const getPayloadClient = async ({ initOptions }: Args) => {
+    if (!process.env.PAYLOAD_SECRET) {
+        throw new Error("PAYLOAD_SECRET is missing!")
+    }
+
+    if (cached.client) {
+        return cached.client
+    }
+
+    if (cached.promise) {
+        cached.promise = payload.init({
+            secret: process.env.PAYLOAD_SECRET,
+            local: initOptions?.express ? false : true,
+            ...(initOptions || {})
+        })
+    }
+    
+    try {
+        cached.client = await cached.promise
+    } catch (err) {
+        cached.promise = null
+        throw err
+    }
+
+    return cached.client
 }
