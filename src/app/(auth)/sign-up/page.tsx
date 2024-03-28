d895cb9652d5eb999069ec86dfef0d873693191d
DerickMghanga <derickmghanga3329@gmail.com>
2024-03-28 23:59:22 +0300

    sign-up done

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,14 @@
+import { Icons } from "@/components/Icons";
+
 export default function page() {
-  return (
-    <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-      page
-      </div>
-    </div>
-  )
+    return (
+        <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+            <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                <div className="flex flex-col items-center space-y-2 text-center">
+                    <Icons.logo className="h-20 w-20" />
+                    <h1 className="text-2xl font-bold">Create an account</h1>
+                </div>
+            </div>
+        </div>
+    )
 }
